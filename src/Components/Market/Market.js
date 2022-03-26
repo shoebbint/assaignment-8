dd29e15e4d2fdecbd4a4bc07e74e8e872129fbda
USER <shoebbint@gmail.com>
2022-03-26 14:04:49 +0600

    api-added

--- src/Components/Market/Market.js
+++ src/Components/Market/Market.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Products from '../Products/Products';
 import { Row } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
+import Cart from '../cart/Cart';
 
 const Market = () => {
+    const [products, setproducts] = useState([]);
+    useEffect(() => {
+
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [])
     return (
-        <div className=''>
+        <div className='container'>
 
             <Row>
                 <Col xs={13} md={9}>
                     <Products></Products>
                 </Col>
                 <Col xs={5} md={3}>
-                    xs=6 md=4
+                    <Cart></Cart>
                 </Col>
             </Row>
         </div>
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 
 const Products = () => {
     return (
         <div>
-            <div className='card'>
-                <img src="" className='card-img-top' alt="" />
-                <div className='card-body'>
-                    <h5 className='card-title'>Card title</h5>
-                    <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className='btn btn-primary'>Go somewhere</a>
-                </div>
-            </div>
+            <Row>
+                <Col xs={6} md={4}>
+                    <div className='card'>
+                        <img src="" className='card-img-top' alt="" />
+                        <div className='card-body'>
+                            <h5 className='card-title'>Card title</h5>
+                            <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" className='btn btn-primary'>Go somewhere</a>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+
         </div>
     );
 };
